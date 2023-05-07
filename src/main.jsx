05605415ecea4050e4ed84acdcb6c866c71133a0
Bronze14 <marcindadek@gmail.com>
2023-05-07 22:38:18 +0200

    css update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import {store} from './store';
 import { Provider } from 'react-redux';
+import 'Style.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
